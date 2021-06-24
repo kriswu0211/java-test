b9eca20b9fa6f800478c9fec2ff036926c9bdb54
wzf <wzf@sams.com>
2021-06-24 19:12:45 +0800

    modify this java

--- git-test-project/src/cn/sams/git/FirstClass.java
+++ git-test-project/src/cn/sams/git/FirstClass.java
@@ -4,4 +4,11 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
+
 }
